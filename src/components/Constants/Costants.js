4117338d32835580b864ff80049089f36ad898cf
Jagpreet SIngh <preetjag999@gmail.com>
2023-03-12 00:02:34 -0700

    changed names

--- src/components/Constants/Costants.js
+++ src/components/Constants/Costants.js
@@ -173,23 +173,23 @@ const testimonials = [
   {
     testimonial:
       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
+    name: "Sara",
+    designation: "CEO",
+    company: "Acmerounds Co",
     image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
     testimonial:
       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
+    name: "Rohit",
     designation: "COO",
-    company: "DEF Corp",
+    company: "DEFRINGS Corp",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
+    name: "Ranveer Wang",
     designation: "CTO",
     company: "456 Enterprises",
     image: "https://randomuser.me/api/portraits/women/6.jpg",
